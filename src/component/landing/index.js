4dbf43f08cf475526607c7ba7a577284457d97e3
Grace-Shao <graceshao203@gmail.com>
2023-02-17 17:45:37 -0500

    [Cynthia's help] left and right side implemented with extra css

--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './styles.module.css';
 
+function LeftSide() {
+  return (
+    <div className={styles.leftside}> left side </div>
+  );
+}
+
 function LandingPage() {
   // store input text into variables
   const [username, setUsername] = React.useState();
@@ -11,16 +17,23 @@ function LandingPage() {
   // we call in the  onClick fuction in button
   function signIn() {
     // eslint-disable-next-line no-alert
+    // 26 min in, implementing axios call to backend
     alert('Not implemented');
   }
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <div className={styles.classone}>
+      <LeftSide/>
+      <div>
+        right side
+        {/*  */}
         <p className={styles.dummyStyle}>Landing page</p>
-        <input type="text" value={username} onChange={(event) => { setUsername(event?.target?.value); }}/>
-        <input type="password" value={password} onChange={(event) => { setPassword(event?.target?.value); }}/>
-        <button onClick={() => { signIn(); }}>Sign In</button>
-        <a href='/sign-in'>Sign In</a>
-        <a href='/sign-up'>Sign Up</a>
+          <input type="text" value={username} onChange={(event) => { setUsername(event?.target?.value); }}/>
+          <input type="password" value={password} onChange={(event) => { setPassword(event?.target?.value); }}/>
+          <button className ={styles.buttonStyle} onClick={() => { signIn(); }}>Sign In</button>
+          <a href='/sign-in'>Sign In</a>
+          <a href='/sign-up'>Sign Up</a>
+      </div>
     </div>
   );
 }
