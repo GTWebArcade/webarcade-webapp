de26b2a115b3620661c804b7c8b65a0c036c7456
Grace Shao <77251173+Grace-Shao@users.noreply.github.com>
2023-02-18 14:51:30 -0500

    Landing page Basic Style Updates (#13)
    * basics of Deepak's username, password set up
    
    * [Cynthia's help] left and right side implemented with extra css
    
    * installed react bootstrap, added some new text
    
    * Updated Landing screen css, not the most prettiest
    
    * moved //eslint-disable-next-line comment up

--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -1,11 +1,46 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 import styles from './styles.module.css';
 
-function LandingPage() {
+function LeftSide() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <div className={styles.leftside}>
+      <h2>GT Web Dev's</h2>
+      <h1>Web Arcade</h1>
+      <p className={styles.bodyText}>Enter the Georgia Tech Web Dev Web Arcade to play
+         and rate user-made WebGL games or upload your own!</p>
+    </div>
+  );
+}
+
+function LandingPage() {
+  // store input text into variables
+  const [username, setUsername] = React.useState();
+  const [password, setPassword] = React.useState();
+  // event?.target?.value store the value of the input field
+  // value={username} the text in the username box (not the actual username)
+
+  // we call in the  onClick fuction in button
+  function signIn() {
+    // 26 min in, implementing axios call to backend
+    // eslint-disable-next-line no-alert
+    alert('Not implemented');
+  }
+
+  return (
+    <div className={styles.classone}>
+      <LeftSide/>
+      <div>
+        right side
+        {/*  */}
         <p className={styles.dummyStyle}>Landing page</p>
-        <a href='/sign-in'>Sign In</a>
-        <a href='/sign-up'>Sign Up</a>
+          <input type="text" value={username} onChange={(event) => { setUsername(event?.target?.value); }}/>
+          <input type="password" value={password} onChange={(event) => { setPassword(event?.target?.value); }}/>
+          <button className ={styles.buttonStyle} onClick={() => { signIn(); }}>Sign In</button>
+          <Button onClick={() => { signIn(); }}variant="primary">Sign In</Button>
+          <a href='/sign-in'>Sign In</a>
+          <a href='/sign-up'>Sign Up</a>
+      </div>
     </div>
   );
 }
