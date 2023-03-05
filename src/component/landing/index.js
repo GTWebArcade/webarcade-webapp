a782d2a8a443d99d85ec19a014c3ecb83ba143b5
Grace-Shao <gshao8@gatech.edu>
2023-03-04 22:56:16 -0500

    Axios point created, passes username and password

--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 import styles from './styles.module.css';
 
 function LeftSide() {
@@ -23,20 +24,33 @@ function RightSide() {
   // event?.target?.value store the value of the input field
   // value={username} the text in the username box (not the actual username)
 
-  // we call in the  onClick fuction in button
+  // we call in the onClick fuction in button
   function signIn() {
-    // 26 min in, implementing axios call to backend
-    // eslint-disable-next-line no-alert
-    alert('Not implemented');
+    // axios call to backend server
+    // url of sign in endpoint
+    axios.post('http://localhost:8080/api/v1/sign-in', {
+      // passes this info to the backend
+      username,
+      password,
+      // response
+    }).then((res) => {
+      // eslint-disable-next-line no-alert
+      alert(JSON.stringify(res.data));
+      // TODO: store access token in local storage
+    }).catch((err) => {
+      // eslint-disable-next-line no-alert
+      alert('Error communicating with the server');
+      console.error(err);
+    });
   }
   return (
     <div className={styles.rightSide}>
         <div className={styles.box}>
           <div className={styles.signInBox}>
           <p>Log In</p>
-            <label className={styles.inline}>Password</label>
-            <input type="text" value={username} onChange={(event) => { setUsername(event?.target?.value); }}/>
             <label className={styles.inline}>Username</label>
+            <input type="text" value={username} onChange={(event) => { setUsername(event?.target?.value); }}/>
+            <label className={styles.inline}>Password</label>
             <input type="password" value={password} onChange={(event) => { setPassword(event?.target?.value); }}/>
             <Button onClick={() => { signIn(); }}variant="primary">Sign In</Button>
           </div>
