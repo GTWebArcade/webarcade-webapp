f47c197ea5f0a5f5f7dceeb8cabc32e6d0e50b3e
Grace-Shao <graceshao203@gmail.com>
2023-03-14 19:27:57 -0400

    Dynamic Device Pixel Ratio implemented

--- src/component/game-loaded/index.js
+++ src/component/game-loaded/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Unity, useUnityContext } from 'react-unity-webgl';
 
@@ -10,9 +10,43 @@ function GameLoadedPage() {
     codeUrl: 'CookingGame/Build/v10 webgl (final build).wasm',
   });
 
+  // We'll use a state to store the device pixel ratio.
+  const [devicePixelRatio, setDevicePixelRatio] = useState(
+    window.devicePixelRatio,
+  );
+
+  const handleChangePixelRatio = useCallback(
+    () => {
+      // A function which will update the device pixel ratio of the Unity
+      // Application to match the device pixel ratio of the browser.
+      const updateDevicePixelRatio = () => {
+        setDevicePixelRatio(window.devicePixelRatio);
+      };
+      // A media matcher which watches for changes in the device pixel ratio.
+      const mediaMatcher = window.matchMedia(
+        `screen and (resolution: ${devicePixelRatio}dppx)`,
+      );
+      // Adding an event listener to the media matcher which will update the
+      // device pixel ratio of the Unity Application when the device pixel
+      // ratio changes.
+      mediaMatcher.addEventListener('change', updateDevicePixelRatio);
+      return () => {
+        // Removing the event listener when the component unmounts.
+        mediaMatcher.removeEventListener('change', updateDevicePixelRatio);
+      };
+    },
+    [devicePixelRatio],
+  );
+
+  useEffect(() => {
+    handleChangePixelRatio();
+  }, [handleChangePixelRatio]);
+
   return (<div>
     Hello world
-    <Unity unityProvider={unityProvider} />
+    <Unity unityProvider={unityProvider}
+    style={{ width: 800, height: 600 }}
+    devicePixelRatio={devicePixelRatio} />
   </div>);
 }
 export default GameLoadedPage;
