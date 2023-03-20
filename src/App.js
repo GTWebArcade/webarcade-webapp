d7b9c580376bb19dc2663829424d1e6bd8c024bc
Grace Shao <77251173+Grace-Shao@users.noreply.github.com>
2023-03-19 21:37:00 -0400

    17 Create page to view a game for the website and load a unity game (#25)
    * created gameLoaded component, uploaded webgl game
    
    * renamed folder in public to CookingGame, game loads in game-loader pg
    
    * updates to eslintIgnore to ignore public folder
    
    * Dynamic Device Pixel Ratio implemented

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './component/landing';
 import SignUpPage from './component/sign-up';
 import GameViewPage from './component/game-view';
+import GameLoadedPage from './component/game-loaded';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <LandingPage/> } />
+        <Route path="" element={ <LandingPage/> } />
         <Route path="sign-up" element={ <SignUpPage/> } />
         <Route path="game-view" element={ <GameViewPage/> } />
+        <Route path="game-loaded" element={ <GameLoadedPage/> } />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/component/game-loaded/index.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect, useCallback } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Unity, useUnityContext } from 'react-unity-webgl';
+
+function GameLoadedPage() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: 'CookingGame/Build/v10 webgl (final build).loader.js',
+    dataUrl: 'CookingGame/Build/v10 webgl (final build).data',
+    frameworkUrl: 'CookingGame/Build/v10 webgl (final build).framework.js',
+    codeUrl: 'CookingGame/Build/v10 webgl (final build).wasm',
+  });
+
+  // We'll use a state to store the device pixel ratio.
+  const [devicePixelRatio, setDevicePixelRatio] = useState(
+    window.devicePixelRatio,
+  );
+
+  const handleChangePixelRatio = useCallback(
+    () => {
+      // A function which will update the device pixel ratio of the Unity
+      // Application to match the device pixel ratio of the browser.
+      const updateDevicePixelRatio = () => {
+        setDevicePixelRatio(window.devicePixelRatio);
+      };
+      // A media matcher which watches for changes in the device pixel ratio.
+      const mediaMatcher = window.matchMedia(
+        `screen and (resolution: ${devicePixelRatio}dppx)`,
+      );
+      // Adding an event listener to the media matcher which will update the
+      // device pixel ratio of the Unity Application when the device pixel
+      // ratio changes.
+      mediaMatcher.addEventListener('change', updateDevicePixelRatio);
+      return () => {
+        // Removing the event listener when the component unmounts.
+        mediaMatcher.removeEventListener('change', updateDevicePixelRatio);
+      };
+    },
+    [devicePixelRatio],
+  );
+
+  useEffect(() => {
+    handleChangePixelRatio();
+  }, [handleChangePixelRatio]);
+
+  return (<div>
+    Hello world
+    <Unity unityProvider={unityProvider}
+    style={{ width: 800, height: 600 }}
+    devicePixelRatio={devicePixelRatio} />
+  </div>);
+}
+export default GameLoadedPage;
