281b53cddb5c4065cb76f161c98d58930ca3a1c6
Grace-Shao <gshao8@gatech.edu>
2023-04-04 19:49:31 -0400

    added props (a few bugs)

--- src/component/game-loaded/index.js
+++ src/component/game-loaded/index.js
@@ -7,22 +7,44 @@ import {
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Unity, useUnityContext } from 'react-unity-webgl';
 import styles from './styles.module.css';
+import { API_URL } from '../../api';
 // import { getPost, getPosts } from './api';
+function UnityWrapper(props) {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: props.loaderUrl,
+    dataUrl: props.dataUrl,
+    frameworkUrl: props.frameworkUrl,
+    codeUrl: props.codeUrl,
+  });
+  return (<Unity unityProvider={unityProvider}
+  style={{ width: 800, height: 600 }}
+  devicePixelRatio={devicePixelRatio} />);
+}
 
 function GameLoadedPage() {
+  // const [unityProvider, setUnityProvider] = useState(undefined);
+  /*
   const { unityProvider } = useUnityContext({
-    loaderUrl: 'CookingGame/Build/v10 webgl (final build).loader.js',
+    loaderUrl: 'CookingGame/Build/v10 ,
     dataUrl: 'CookingGame/Build/v10 webgl (final build).data',
     frameworkUrl: 'CookingGame/Build/v10 webgl (final build).framework.js',
     codeUrl: 'CookingGame/Build/v10 webgl (final build).wasm',
   });
+  */
 
+  // did i write this right?
+  const [game, setGame] = useState(undefined);
   const { id } = useParams();
   console.log(id);
-  // {curly brace should be api url}
-  axios.get(`${id}/api/v1/${id}`).then((res) => {
-    console.log(res.data);
-  });
+  useEffect(() => {
+    console.log('render');
+    const url = `${API_URL}/api/v1/game/${id}`;
+    console.log('url', url);
+    axios.get(url).then((res) => {
+      console.log('game info:', res.data);
+      setGame(res.data);
+    });
+  }, [id]);
 
   // We'll use a state to store the device pixel ratio.
   const [devicePixelRatio, setDevicePixelRatio] = useState(
@@ -69,9 +91,11 @@ function GameLoadedPage() {
       </h1>
       <button className={styles.button} onClick={navigateLanding}>Log Out</button>
     </span>
-    <Unity unityProvider={unityProvider}
-    style={{ width: 800, height: 600 }}
-    devicePixelRatio={devicePixelRatio} />
+    {
+      game && <UnityWrapper loaderUrl={game.unityLoaderUrl}
+      dataUrl={game.unityDataUrl}
+      frameworkUrl={game.unityFrameworkUrl} codeUrl={game.unityCodeUrl} />
+    }
   </div>);
 }
 export default GameLoadedPage;
